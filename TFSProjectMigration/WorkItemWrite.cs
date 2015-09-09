7893ecc231930289be9cb1d3451f0c1712d2d563
Philippe Miossec <pmiossec@gmail.com>
2015-09-10 00:29:24 +0200

    Force creation of workitems (with heuristically found data) instead of failing

--- TFSProjectMigration/WorkItemWrite.cs
+++ TFSProjectMigration/WorkItemWrite.cs
@@ -217,12 +217,30 @@ namespace TFSProjectMigration
                     }
                 }
 
+                bool correctErrorsAbsolutely = false;
                 /* Validate Item Before Save*/
                 ArrayList array = newWorkItem.Validate();
+                bool isInError = array.Count != 0;
                 foreach (Field item in array)
                 {
-                        logger.Info(String.Format("Work item {0} Validation Error in field: {1}  : {2}", workItem.Id, item.Name, newWorkItem.Fields[item.Name].Value));
+                    if (!correctErrorsAbsolutely)
+                    {
+                        logger.WarnFormat("Work item {0} Validation Error in field: {1}  : {2}", workItem.Id,
+                            item.Name, newWorkItem.Fields[item.Name].Value);
+                    }
+                    else
+                    {
+                        var initialValue = item.Value.ToString();
+                        var allowedValueSortedByAcceptability =
+                            item.AllowedValues.OfType<string>().OrderBy(e => LevenshteinDistance.Compute(initialValue, e));
+                        var value = allowedValueSortedByAcceptability.First();
+                        item.Value = value;
+                        logger.WarnFormat("Work item {0} Validation Error in field: {1}  : {2}=> Replaced by value: {3}", workItem.Id,
+                            item.Name, initialValue, value);
+                    }
                 }
+                if (isInError && correctErrorsAbsolutely)
+                    array = newWorkItem.Validate();
                 //if work item is valid
                 if (array.Count == 0)
                 {
@@ -249,6 +267,47 @@ namespace TFSProjectMigration
             WriteMaptoFile(sourceProjectName);
             CreateLinks(newItems, sourceStore);
         }
+
+
+        static class LevenshteinDistance
+        {
+            public static int Compute(string s, string t)
+            {
+                if (string.IsNullOrEmpty(s))
+                {
+                    if (string.IsNullOrEmpty(t))
+                        return 0;
+                    return t.Length;
+                }
+
+                if (string.IsNullOrEmpty(t))
+                {
+                    return s.Length;
+                }
+
+                int n = s.Length;
+                int m = t.Length;
+                int[,] d = new int[n + 1, m + 1];
+
+                // initialize the top and right of the table to 0, 1, 2, ...
+                for (int i = 0; i <= n; d[i, 0] = i++) ;
+                for (int j = 1; j <= m; d[0, j] = j++) ;
+
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int j = 1; j <= m; j++)
+                    {
+                        int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+                        int min1 = d[i - 1, j] + 1;
+                        int min2 = d[i, j - 1] + 1;
+                        int min3 = d[i - 1, j - 1] + cost;
+                        d[i, j] = Math.Min(Math.Min(min1, min2), min3);
+                    }
+                }
+                return d[n, m];
+            }
+        }
+
 
         private Hashtable ListToTable(List<object> map)
         {
